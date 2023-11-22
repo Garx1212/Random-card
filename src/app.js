d1ca15601df142ccde5d50f5996a9a98f4d2743a
Garx1212 <113130182+Garx1212@users.noreply.github.com>
2023-11-22 02:09:11 +0000

    commit

--- src/app.js
+++ src/app.js
@@ -2,10 +2,33 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let theCard = document.getElementById("theCard");
+
+  let randomNum = Math.floor(Math.random() * 13 + 1);
+
+  let icon = "a";
+  let iconReverse = "a";
+  let specialCharacter = "a";
+
+  if (randomNum === 1) {
+    icon = `"fa-solid fa-heart" style="color: #f50000;"`;
+    iconReverse = `"fa-solid fa-heart fa-rotate-180" style="color: #f50000;"`;
+    specialCharacter = "A";
+  }
+
+  let thisHTML = `<div class="d-flex justify-content-start p-2">
+    <i class='${icon} mb-8'></i>
+    </div>
+    
+    <div>
+    <h1 class="d-flex justify-content-center">${specialCharacter}</h1>
+    </div>
+    
+    <div class="d-flex justify-content-end p-2">
+    <i class='${iconReverse} mt-8'></i>
+    </div>`;
+
+  theCard.innerHTML = thisHTML;
+  console.log(Math.floor(Math.random() * 13 + 1));
 };
